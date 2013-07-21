125b4d632b14660d5a5a2f87b85b092add0355e0
Shamir Khodzha <khodzha.sh@gmail.com>
2013-07-21 13:17:15 +0400

    implemented some peer messages

--- peer.rb
+++ peer.rb
@@ -1,17 +1,20 @@
 class Peer
-	def initialize(socket)
+	def initialize(socket, pieces, piece_length)
 		@socket = socket
 		@am_interested = false
 		@am_choking = true
 
 		@peer_interested = false
 		@peer_choking = true
+		@pieces = pieces.each_with_index.inject([]) {|r, (v, index)| r[index] = {hashsum: v, have: false}; r}
+		@piece_length = piece_length
 	end
 
-	def start pieces, piece_length
+	def start 
 		Thread.new do
 			puts "starting data transmission for #{@socket.peeraddr[2]}"
 			Thread.new do
+				# keep alive
 				@socket.send [0].pack('L')
 				sleep 120
 			end
@@ -30,8 +33,11 @@ class Peer
 					@peer_interested = false
 				when 4
 					# have
+					piece_index = message.unpack('C')[0]
+					@pieces[piece_index][:have] = true if @pieces[piece_index]
 				when 5
 					# bitfield
+					bitfield_to_array message
 				when 6
 					#request
 				when 7
@@ -39,6 +45,20 @@ class Peer
 				when 8
 					# cancel
 				end
+			end
+		end
+	end
+
+	private
+
+	def bitfield_to_array bitfield
+		index = 0
+		bitfield.unpack('C*').each do |byte|
+			0.upto(7).each do |offset|
+				if @pieces[index] && (byte & (1<<offset) == 1)
+					@pieces[index][:have] = true
+				end
+				index+=1
 			end
 		end
 	end
