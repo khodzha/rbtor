cb0e5c6eac1aab6eb3a8350a0e0a9871e986107a
Shamir Khodzha <khodzha.sh@gmail.com>
2013-08-07 20:38:34 +0400

    converted tabs to spaces

--- peer.rb
+++ peer.rb
@@ -4,224 +4,224 @@ require 'forwardable'
 $logging = false
 
 class Peer
-	extend Forwardable
-	def_delegators :@torrent, :mutex
+  extend Forwardable
+  def_delegators :@torrent, :mutex
 
-	BLOCK_SIZE = 2**14
-	MAX_REQUESTS = 5
+  BLOCK_SIZE = 2**14
+  MAX_REQUESTS = 5
 
-	def initialize(socket, pieces, piece_length, torrent)
-		@socket = socket
-		@am_interested = false
-		@am_choking = true
+  def initialize(socket, pieces, piece_length, torrent)
+    @socket = socket
+    @am_interested = false
+    @am_choking = true
 
-		@peer_interested = false
-		@peer_choking = true
-		@pieces = pieces
-		@piece_length = piece_length
-		@torrent = torrent
-		@peeraddr = @socket.peeraddr[2]
-		@threads = []
-		@last_send = Time.now
-		@last_receive = Time.now
-		@downloading_piece = nil
-		@current_requests = 0
-		@shutdown_flag = false
-	end
+    @peer_interested = false
+    @peer_choking = true
+    @pieces = pieces
+    @piece_length = piece_length
+    @torrent = torrent
+    @peeraddr = @socket.peeraddr[2]
+    @threads = []
+    @last_send = Time.now
+    @last_receive = Time.now
+    @downloading_piece = nil
+    @current_requests = 0
+    @shutdown_flag = false
+  end
 
-	def to_s
-		peer_str = @peeraddr.to_s
-		peer_str << "\t" if peer_str.size < 12
-		"#{peer_str}\t#{@peer_choking}\t#{@peer_interested}\t"
-	end
+  def to_s
+    peer_str = @peeraddr.to_s
+    peer_str << "\t" if peer_str.size < 12
+    "#{peer_str}\t#{@peer_choking}\t#{@peer_interested}\t"
+  end
 
-	def inspect
-		"<#{self.to_s}>"
-	end
+  def inspect
+    "<#{self.to_s}>"
+  end
 
-	def start
-		send_bitfield
-		send_interested
-		send_unchoking
+  def start
+    send_bitfield
+    send_interested
+    send_unchoking
 
-		@threads << Thread.new do
-			until @shutdown_flag
-				# keep alive
-				sleep 30
-				if Time.now.to_i - @last_send.to_i > 90
-					mutex.synchronize do
-						@last_send = Time.now
-					end
-					data = [0, 0, 0, 0].pack('C4')
-					puts "KEEP ALIVE message: #{data.inspect}"
-					@socket.print data
-				end
-				if Time.now.to_i - @last_receive.to_i > 180
-					mutex.synchronize do
-						@current_requests = nil
-						@downloading_piece[:downloading] = false if @downloading_piece
-						@downloading_piece = nil
-						@torrent.remove_peer self
-						@shutdown_flag = true
-						@socket.close
-					end
-				end
-			end
-		end
+    @threads << Thread.new do
+      until @shutdown_flag
+        # keep alive
+        sleep 30
+        if Time.now.to_i - @last_send.to_i > 90
+          mutex.synchronize do
+            @last_send = Time.now
+          end
+          data = [0, 0, 0, 0].pack('C4')
+          puts "KEEP ALIVE message: #{data.inspect}"
+          @socket.print data
+        end
+        if Time.now.to_i - @last_receive.to_i > 180
+          mutex.synchronize do
+            @current_requests = nil
+            @downloading_piece[:downloading] = false if @downloading_piece
+            @downloading_piece = nil
+            @torrent.remove_peer self
+            @shutdown_flag = true
+            @socket.close
+          end
+        end
+      end
+    end
 
-		@threads << Thread.new do
-			until @shutdown_flag
-				sleep 2
-				puts "#{time} #{self} #{@socket.closed?}" if $logging
-				puts "#{time} #{self} THREADS_STATUS: #{@threads.map(&:status).inspect}" if $logging
-			end
-		end
+    @threads << Thread.new do
+      until @shutdown_flag
+        sleep 2
+        puts "#{time} #{self} #{@socket.closed?}" if $logging
+        puts "#{time} #{self} THREADS_STATUS: #{@threads.map(&:status).inspect}" if $logging
+      end
+    end
 
-		@threads << Thread.new do
-			until @shutdown_flag
-				if @peer_choking == false
-					if @downloading_piece.nil? || ( @current_requests < MAX_REQUESTS && @downloading_piece[:blocks_downloaded].any?{|x| x == :not_downloaded} )
-						@downloading_piece = @torrent.get_piece_for_downloading self unless @downloading_piece
-						if @downloading_piece
-							puts "#{time} #{self} PIECE DL index: #{@downloading_piece[:index].inspect}"
-							send_requests
-						end
-					end
-				end
-				sleep 1
-			end
-		end
+    @threads << Thread.new do
+      until @shutdown_flag
+        if @peer_choking == false
+          if @downloading_piece.nil? || ( @current_requests < MAX_REQUESTS && @downloading_piece[:blocks_downloaded].any?{|x| x == :not_downloaded} )
+            @downloading_piece = @torrent.get_piece_for_downloading self unless @downloading_piece
+            if @downloading_piece
+              puts "#{time} #{self} PIECE DL index: #{@downloading_piece[:index].inspect}"
+              send_requests
+            end
+          end
+        end
+        sleep 1
+      end
+    end
 
-		@threads << Thread.new do
-			until @shutdown_flag
-				@socket.wait_readable
-				puts "#{time} #{self} bytes: #{@socket.nread.inspect}"
-				while @socket.nread < 4
-					sleep 1
-				end
-				message_len = @socket.recv(4).unpack('L>')[0]
-				next if message_len.nil?
-				puts "MESSAGE_LEN: #{message_len}"
-				message = receive_message message_len
-				message_id, payload = message.unpack('Ca*')
-				puts "#{time} #{self} #{message_len} #{message_id} #{payload.unpack('C*').take(40)}"
-				case message_id
-				when 0
-					@peer_choking = true
-				when 1
-					@peer_choking = false
-				when 2
-					@peer_interested = trueresponse
-				when 3
-					@peer_interested = false
-				when 4
-					# have
-					piece_index = payload.unpack('L>')[0]
-					@torrent.update_pieces self, piece_index
-				when 5
-					# bitfield
-					bitfield_to_array payload
-				when 6
-					# request
-					index, start, length = payload.unpack('L>L>L>')
-					data = @torrent.get_piece(index, start, length)
-					puts "REQUEST response: " + data.inspect
-					@socket.print data
-				when 7
-					# piece
-					index, start, data = payload.unpack('L>L>a*')
-					mutex.synchronize do
-						@torrent.save_piece self, index, start, data
-						@current_requests -= 1
-						@downloading_piece = nil if @current_requests == 0 && @downloading_piece[:blocks_downloaded].all?{|x| x == :downloaded}
-					end
-				when 8
-					# cancel
-				else
-				end
-			end
-		end
-		puts "THREADS_SIZE= #{@threads.size.inspect}"
-		@threads
-	end
+    @threads << Thread.new do
+      until @shutdown_flag
+        @socket.wait_readable
+        puts "#{time} #{self} bytes: #{@socket.nread.inspect}"
+        while @socket.nread < 4
+          sleep 1
+        end
+        message_len = @socket.recv(4).unpack('L>')[0]
+        next if message_len.nil?
+        puts "MESSAGE_LEN: #{message_len}"
+        message = receive_message message_len
+        message_id, payload = message.unpack('Ca*')
+        puts "#{time} #{self} #{message_len} #{message_id} #{payload.unpack('C*').take(40)}"
+        case message_id
+        when 0
+          @peer_choking = true
+        when 1
+          @peer_choking = false
+        when 2
+          @peer_interested = trueresponse
+        when 3
+          @peer_interested = false
+        when 4
+          # have
+          piece_index = payload.unpack('L>')[0]
+          @torrent.update_pieces self, piece_index
+        when 5
+          # bitfield
+          bitfield_to_array payload
+        when 6
+          # request
+          index, start, length = payload.unpack('L>L>L>')
+          data = @torrent.get_piece(index, start, length)
+          puts "REQUEST response: " + data.inspect
+          @socket.print data
+        when 7
+          # piece
+          index, start, data = payload.unpack('L>L>a*')
+          mutex.synchronize do
+            @torrent.save_piece self, index, start, data
+            @current_requests -= 1
+            @downloading_piece = nil if @current_requests == 0 && @downloading_piece[:blocks_downloaded].all?{|x| x == :downloaded}
+          end
+        when 8
+          # cancel
+        else
+        end
+      end
+    end
+    puts "THREADS_SIZE= #{@threads.size.inspect}"
+    @threads
+  end
 
-	def send_have index
-		data = [5, 4, index].pack('L>CL>')
-		@socket.print data
-	end
+  def send_have index
+    data = [5, 4, index].pack('L>CL>')
+    @socket.print data
+  end
 
-	private
+  private
 
-	def send_bitfield
-		mutex.synchronize do
-			@last_send = Time.now
-		end
-		bitfield_size = (@pieces.size/8.0).ceil
-		data = [ bitfield_size + 1, 5, [0]*bitfield_size].flatten
-		puts "BITFIELD message: #{data}"
-		@socket.print data.pack('L>C*')
-	end
+  def send_bitfield
+    mutex.synchronize do
+      @last_send = Time.now
+    end
+    bitfield_size = (@pieces.size/8.0).ceil
+    data = [ bitfield_size + 1, 5, [0]*bitfield_size].flatten
+    puts "BITFIELD message: #{data}"
+    @socket.print data.pack('L>C*')
+  end
 
-	def bitfield_to_array bitfield
-		# OPTIMIZE need to rewrite with something like merge ?
-		index = 0
-		bitfield.unpack('C*').each do |byte|
-			0.upto(7).each do |offset|
-				if (byte & (1<<offset) == 1)
-					@torrent.update_pieces self, index
-				end
-				index+=1
-			end
-		end
-	end
+  def bitfield_to_array bitfield
+    # OPTIMIZE need to rewrite with something like merge ?
+    index = 0
+    bitfield.unpack('C*').each do |byte|
+      0.upto(7).each do |offset|
+        if (byte & (1<<offset) == 1)
+          @torrent.update_pieces self, index
+        end
+        index+=1
+      end
+    end
+  end
 
-	def send_unchoking
-		mutex.synchronize do
-			@last_send = Time.now
-		end
-		data = [1, 1].pack('L>C')
-		puts "UNCHOKE message: " + data.inspect
-		@socket.print data
-	end
+  def send_unchoking
+    mutex.synchronize do
+      @last_send = Time.now
+    end
+    data = [1, 1].pack('L>C')
+    puts "UNCHOKE message: " + data.inspect
+    @socket.print data
+  end
 
-	def send_interested
-		mutex.synchronize do
-			@last_send = Time.now
-		end
-		data = [1, 2].pack('L>C')
-		puts "INTERESTED message: " + data.inspect
-		@socket.print data
-	end
+  def send_interested
+    mutex.synchronize do
+      @last_send = Time.now
+    end
+    data = [1, 2].pack('L>C')
+    puts "INTERESTED message: " + data.inspect
+    @socket.print data
+  end
 
-	def send_requests
-		mutex.synchronize do
-			(MAX_REQUESTS - @current_requests).times do |i|
-				index = @downloading_piece[:blocks_downloaded].find_index(:not_downloaded)
-				break if index.nil?
-				@downloading_piece[:blocks_downloaded][index] = :in_progress
-				data = [13, 6, @downloading_piece[:index], index*BLOCK_SIZE, BLOCK_SIZE]
-				puts "REQUEST message: #{data.inspect}"
-				@socket.print data.pack('L>CL>3')
-				@current_requests += 1
-			end
+  def send_requests
+    mutex.synchronize do
+      (MAX_REQUESTS - @current_requests).times do |i|
+        index = @downloading_piece[:blocks_downloaded].find_index(:not_downloaded)
+        break if index.nil?
+        @downloading_piece[:blocks_downloaded][index] = :in_progress
+        data = [13, 6, @downloading_piece[:index], index*BLOCK_SIZE, BLOCK_SIZE]
+        puts "REQUEST message: #{data.inspect}"
+        @socket.print data.pack('L>CL>3')
+        @current_requests += 1
+      end
 
-			@last_send = Time.now
-		end
-	end
+      @last_send = Time.now
+    end
+  end
 
-	def time
-		Time.now.strftime('%T')+"\t"
-	end
+  def time
+    Time.now.strftime('%T')+"\t"
+  end
 
-	def receive_message message_len
-		message = ''
-		while message_len > 0
-			@socket.wait_readable
-			buf = @socket.recv message_len
-			message_len -= buf.size
-			message << buf
-		end
-		@last_receive = Time.now
-		message
-	end
+  def receive_message message_len
+    message = ''
+    while message_len > 0
+      @socket.wait_readable
+      buf = @socket.recv message_len
+      message_len -= buf.size
+      message << buf
+    end
+    @last_receive = Time.now
+    message
+  end
 end
--- torrent.rb
+++ torrent.rb
@@ -7,128 +7,128 @@ require './bencode'
 require './peer'
 
 class Torrent
-	attr_reader :mutex, :piece_length
+  attr_reader :mutex, :piece_length
 
-	def initialize filename
-		@ben = Bencode.new(filename)
-		@data = @ben.decode
+  def initialize filename
+    @ben = Bencode.new(filename)
+    @data = @ben.decode
 
-		unpack_format = 'a20'*(@data[:info][:pieces].size/20)
-		@pieces = @data[:info][:pieces].unpack(unpack_format)
-		@piece_length = @data[:info][:"piece length"]
+    unpack_format = 'a20'*(@data[:info][:pieces].size/20)
+    @pieces = @data[:info][:pieces].unpack(unpack_format)
+    @piece_length = @data[:info][:"piece length"]
 
-		params = { 	peer_id: '-RB0001-000000000001', event: 'started', info_hash: @ben.info_hash.scan(/../).map(&:hex).pack('c*'),
-					port: 6881, uploaded: 0, downloaded: 0, left: @data[:info][:length]
-				}
-		@uri = URI @data[:announce]
-		@uri.query = URI.encode_www_form params
+    params = {  peer_id: '-RB0001-000000000001', event: 'started', info_hash: @ben.info_hash.scan(/../).map(&:hex).pack('c*'),
+          port: 6881, uploaded: 0, downloaded: 0, left: @data[:info][:length]
+        }
+    @uri = URI @data[:announce]
+    @uri.query = URI.encode_www_form params
 
-		@peers = []
-		@mutex = Mutex.new
-		@downloaded_pieces = []
-		@tracker_data = nil
-	end
+    @peers = []
+    @mutex = Mutex.new
+    @downloaded_pieces = []
+    @tracker_data = nil
+  end
 
-	def to_s
-		"#{@peers.size} #{@pieces.size} #{@downloaded_pieces.size}"
-	end
+  def to_s
+    "#{@peers.size} #{@pieces.size} #{@downloaded_pieces.size}"
+  end
 
-	def inspect
-		"<#{self.to_s}>"
-	end
+  def inspect
+    "<#{self.to_s}>"
+  end
 
-	def start
-		puts "Connecting to #{@uri.host}"
-		res = Net::HTTP.get_response(@uri).body
-		tracker_ben = Bencode.new StringIO.new(res)
-		@tracker_data = tracker_ben.decode
-		threads = []
+  def start
+    puts "Connecting to #{@uri.host}"
+    res = Net::HTTP.get_response(@uri).body
+    tracker_ben = Bencode.new StringIO.new(res)
+    @tracker_data = tracker_ben.decode
+    threads = []
 
-		@tracker_data[:peers].scan(/.{6}/).take(10).each_with_index do |x, i|
-			threads << Thread.new do
-				t = x.unpack('CCCCS>')
-				host, port = t[0..3].join('.'), t[4]
-				begin
-					Timeout::timeout(5) do
-						socket = TCPSocket.new(host, port)
-						handshake = [19].pack('C') + 'BitTorrent protocol' + [0].pack('Q') + @ben.info_hash.scan(/../).map(&:hex).pack('c*') + '-RB0001-000000000001'
-						socket.print handshake
-						data = socket.recv 49+19
-						response = data.unpack 'CA19Q>C20C20'
-						if response[0] == 19
-							puts response.inspect
-							@mutex.synchronize do
-								@peers << Peer.new(socket, @pieces, @piece_length, self)
-							end
-						end
-					end
-				rescue Errno::ECONNRESET, Errno::ECONNABORTED, Errno::ETIMEDOUT, Errno::ECONNREFUSED, Errno::EHOSTUNREACH, Timeout::Error
-					puts "#{host}:#{port} - failed"
-				end
-			end
-		end
-		threads.map &:join
-		puts 'total connections: ' + @peers.size.to_s
-		exit if @peers.size == 0
+    @tracker_data[:peers].scan(/.{6}/).take(10).each_with_index do |x, i|
+      threads << Thread.new do
+        t = x.unpack('CCCCS>')
+        host, port = t[0..3].join('.'), t[4]
+        begin
+          Timeout::timeout(5) do
+            socket = TCPSocket.new(host, port)
+            handshake = [19].pack('C') + 'BitTorrent protocol' + [0].pack('Q') + @ben.info_hash.scan(/../).map(&:hex).pack('c*') + '-RB0001-000000000001'
+            socket.print handshake
+            data = socket.recv 49+19
+            response = data.unpack 'CA19Q>C20C20'
+            if response[0] == 19
+              puts response.inspect
+              @mutex.synchronize do
+                @peers << Peer.new(socket, @pieces, @piece_length, self)
+              end
+            end
+          end
+        rescue Errno::ECONNRESET, Errno::ECONNABORTED, Errno::ETIMEDOUT, Errno::ECONNREFUSED, Errno::EHOSTUNREACH, Timeout::Error
+          puts "#{host}:#{port} - failed"
+        end
+      end
+    end
+    threads.map &:join
+    puts 'total connections: ' + @peers.size.to_s
+    exit if @peers.size == 0
 
-		@pieces = @pieces.each_with_index.inject([]) {|r, (v, index)| r[index] = {hashsum: v, peers: [], peers_have: 0, index: index, downloading: false}; r}
-		@pieces.each do |piece|
-			piece[:blocks_downloaded] = [:not_downloaded] * ( @piece_length.to_f / Peer::BLOCK_SIZE ).ceil
-		end
+    @pieces = @pieces.each_with_index.inject([]) {|r, (v, index)| r[index] = {hashsum: v, peers: [], peers_have: 0, index: index, downloading: false}; r}
+    @pieces.each do |piece|
+      piece[:blocks_downloaded] = [:not_downloaded] * ( @piece_length.to_f / Peer::BLOCK_SIZE ).ceil
+    end
 
-		@peers.map(&:start).flatten.map &:join
-	end
+    @peers.map(&:start).flatten.map &:join
+  end
 
-	def update_pieces peer, piece_index
-		piece = @pieces[piece_index]
-		@mutex.synchronize do
-			if piece && !piece[:peers].include?(peer)
-				piece[:peers] << peer
-				piece[:peers_have] += 1
-			end
-		end
-	end
+  def update_pieces peer, piece_index
+    piece = @pieces[piece_index]
+    @mutex.synchronize do
+      if piece && !piece[:peers].include?(peer)
+        piece[:peers] << peer
+        piece[:peers_have] += 1
+      end
+    end
+  end
 
-	def save_piece peer, index, start, data
-		puts "SAVE PIECE #{index} #{start}"
-		piece = @pieces[index]
-		block_index = start / Peer::BLOCK_SIZE
+  def save_piece peer, index, start, data
+    puts "SAVE PIECE #{index} #{start}"
+    piece = @pieces[index]
+    block_index = start / Peer::BLOCK_SIZE
 
-		piece[:blocks_downloaded][block_index] = :downloaded
-		piece_downloaded = piece[:blocks_downloaded].any?{|x| x != :downloaded}
+    piece[:blocks_downloaded][block_index] = :downloaded
+    piece_downloaded = piece[:blocks_downloaded].any?{|x| x != :downloaded}
 
-		File.open('./tmp/' + @pieces[index][:hashsum].each_byte.map{|b| "%02X"%b}.join + '.tmp', File::CREAT|File::BINARY|File::WRONLY) do |f|
-			f.seek(start)
-			f.write(data)
-		end
+    File.open('./tmp/' + @pieces[index][:hashsum].each_byte.map{|b| "%02X"%b}.join + '.tmp', File::CREAT|File::BINARY|File::WRONLY) do |f|
+      f.seek(start)
+      f.write(data)
+    end
 
-		@downloaded_pieces << @pieces[index]
-		(@peers-[peer]).each{|x| x.send_have(index)} if piece_downloaded
-	end
+    @downloaded_pieces << @pieces[index]
+    (@peers-[peer]).each{|x| x.send_have(index)} if piece_downloaded
+  end
 
-	def get_piece index, start, length
-		@mutex.synchronize do
-			file_name = './tmp/' + @pieces[index][:hashsum].each_byte.map{|b| "%02x"%b}.join + '.tmp'
-			File.read(file_name, length, start)
-		end
-	end
+  def get_piece index, start, length
+    @mutex.synchronize do
+      file_name = './tmp/' + @pieces[index][:hashsum].each_byte.map{|b| "%02x"%b}.join + '.tmp'
+      File.read(file_name, length, start)
+    end
+  end
 
-	def get_piece_for_downloading peer
-		piece = nil
-		@mutex.synchronize do
-			piece = (@pieces - @downloaded_pieces).select{|x| x[:peers].include?(peer) && x[:downloading] == false}.sort_by{|x| -x[:peers_have]}.first
-			piece[:downloading] = true if piece
-		end
-		puts "PIECE INSPECT: #{piece.inspect}"
-		piece
-	end
+  def get_piece_for_downloading peer
+    piece = nil
+    @mutex.synchronize do
+      piece = (@pieces - @downloaded_pieces).select{|x| x[:peers].include?(peer) && x[:downloading] == false}.sort_by{|x| -x[:peers_have]}.first
+      piece[:downloading] = true if piece
+    end
+    puts "PIECE INSPECT: #{piece.inspect}"
+    piece
+  end
 
-	def remove_peer peer
-		@pieces.select{|x| x[:peers].include?(peer)}.each do |piece|
-			piece[:peers].delete(peer)
-			piece[:peers_have] -= 1
-			piece[:blocks_downloaded].map!{|x| x == (:in_progress ? :not_downloaded : x)}
-		end
-		@peers.delete(peer)
-	end
+  def remove_peer peer
+    @pieces.select{|x| x[:peers].include?(peer)}.each do |piece|
+      piece[:peers].delete(peer)
+      piece[:peers_have] -= 1
+      piece[:blocks_downloaded].map!{|x| x == (:in_progress ? :not_downloaded : x)}
+    end
+    @peers.delete(peer)
+  end
 end
